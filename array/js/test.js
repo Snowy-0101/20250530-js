55e7e2345943c7f925f95a8813cc772b955da960
Snowy-0101 <TTC2350SD0005@edu.tech.ac.jp>
2025-05-30 18:38:52 +0900

    Add files via upload

---
+++ array/js/test.js
@@ -0,0 +1,14 @@
+// Q1 配列の作成
+let fruits = ["apple" , "banana" , "cherry"];
+console.log(fruits);
+
+// Q2 要素の参照
+console.log(fruits[1]);
+
+
+// Q3要素の追加
+let numbers = [1 , 2, 3];
+console.log(numbers);
+
+numbers.push(10);
+console.log(numbers);
---
+++ operator/js/script.js
@@ -0,0 +1,85 @@
+// 加算
+let add = 10 + 5;
+console.log(add); //15
+
+// 文字列の連結
+let str = "Java" + "Script";
+console.log(str);
+
+let result1 = 3 + 3;
+let result2 = "3" + "3";
+
+console.log(result1);
+console.log(result2)
+
+// 減算
+let sub = 10-5;
+console.log(sub); //5
+
+// 乗算
+let mul = 10 * 5 //10 x 5
+console.log(mul); //50
+
+// 除算
+let div = 10 / 5; //10 ÷ 5
+console.log(div); //2
+
+// インクリメント
+let incr = 10; //現在の値に1を加える
+incr++;
+console.log(incr); //11
+
+// デクリメント
+let decr = 10; //現在の値から1を引く
+decr--;
+console.log(decr); //9
+
+/* 代入演算子 */
+
+// 加算して代入
+let add2 = 1;
+add2 += 2;   // 1 + 2
+console.log(add2); //3
+
+// 文字列を連結して代入
+let str2 = "Java"; 
+str2 += "Script"; //"Java" + "Script"
+console.log(str2); // "JavaScript"
+
+// 減算して代入
+let sub2 = 10;
+sub2 -= 5; // 10 - 5
+console.log(sub2); // 5
+
+// 乗算して代入
+let mul2 = 10;
+mul2 *= 5; // 10 x 5
+console.log(mul2); // 50
+
+// 除算して代入
+let div2 = 10;
+div2 /= 5; // 10 ÷ 5
+console.log(div2); // 2
+
+/* 比較演算子 */
+
+// 等しいどうか
+let isEqual = (10 == 10); //値が等しい
+console.log(isEqual); //true
+
+// 等しくないかどうか 
+let isNotEqual = (10 != 5); //値が等しくない
+console.log(isNotEqual); //true
+
+// より大きいかどうか>
+let isGreater = (10 > 5); //10は5より大きい
+console.log(isGreater); //true
+
+// より小さいかどうか
+let isLess = (10 < 1); //10は5より小さい
+console.log(isLess); //false
+
+
+/*<>
+&gt; &lt; //HTMLけーぷ<
+a than b */
